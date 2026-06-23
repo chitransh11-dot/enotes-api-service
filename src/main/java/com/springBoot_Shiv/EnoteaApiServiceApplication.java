08022dede9db614238e073af3323acdcf65e64c6
Chitransh <chitranshtiwari11@gmail.com>
2026-06-23 07:37:36 +0530

    initial commit

---
+++ src/main/java/com/springBoot_Shiv/EnoteaApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot_Shiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnoteaApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnoteaApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springBoot_Shiv/EnoteaApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot_Shiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnoteaApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
